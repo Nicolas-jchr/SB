4b689ca867f1f9a64cff67c7f7a3e93c41618c17
Nicolas-jchr <Nicolas.jchr@gmail.com>
2024-06-18 16:19:07 +0200

    Footer content

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,5 +1,31 @@
 function Footer() {
-  return <h1>Hello i'm Footer PAGE</h1>;
+  return (
+    <footer className="footerContainer">
+      <section className="meetButtonFooter">
+        {" "}
+        <a
+          href="https://www.rdv360.com/sbeauty-30190-st-chaptes"
+          target="_blank"
+          className="meetButtonBottom"
+          rel="noreferrer"
+        >
+          Prendre rendez-vous
+        </a>
+      </section>
+      <section>
+        <h1>Mes réseaux sociaux</h1>
+        <p>Instagram</p>
+        <p>Facebook</p>
+        <p>Tiktok</p>
+      </section>
+      <section>
+        <h1>Me contacter</h1>
+        <p>Institut SBeauty - 10 rue, 30190 Saint-Chaptes</p>
+        <p>téléphone: 07-00-00-00-00</p>
+        <p>e-mail: institut@gmail.com</p>
+      </section>
+    </footer>
+  );
 }
 
 export default Footer;
