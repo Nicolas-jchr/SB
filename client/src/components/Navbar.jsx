5a818b7cc6908c45560c9de126f183c51c892256
Nicolas-jchr <Nicolas.jchr@gmail.com>
2024-06-20 13:20:16 +0200

    Add burger menu in mobile version

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import { NavLink } from "react-router-dom";
+import { slide as Menu } from "react-burger-menu";
 import Logo from "../assets/logosbeauty.png";
-import Home from "../assets/home.png";
 
 function NavBar() {
   return (
@@ -38,35 +38,45 @@ function NavBar() {
           </li>
         </nav>
       </section>
+
       <section className="navbarMobileContainer">
-        <nav>
-          <menu className="navbarMobile">
-            <li>
-              <NavLink to="/">
-                <img src={Home} alt="Icône page accueil" className="navIcon" />
-              </NavLink>
-            </li>
-            <li>
-              <NavLink to="/Services">
-                <img src={Home} alt="page profil" className="navIcon" />
-              </NavLink>
-            </li>
-            <li>
-              <NavLink to="/Avis">
-                <img
-                  src={Home}
-                  alt="ajouter un street art"
-                  className="navIcon"
-                />
-              </NavLink>
-            </li>
-            <li>
-              <NavLink to="/about">
-                <img src={Home} alt="page classement" className="navIcon" />
-              </NavLink>
-            </li>
-          </menu>
-        </nav>
+        <div className="navbarFixedTop">
+          <a
+            href="https://www.rdv360.com/sbeauty-30190-st-chaptes"
+            target="_blank"
+            className="meetButtonMobile"
+            rel="noreferrer"
+          >
+            Prendre rendez-vous
+          </a>
+          <div className="burgerButton">
+            <Menu right>
+              <div className="menuHeader">
+                <NavLink to="/">
+                  <img
+                    src={Logo}
+                    alt="Logo Street Art Hunter"
+                    className="logoMobile"
+                  />
+                </NavLink>
+              </div>
+              <nav className="navbarBurger">
+                <li className="navbarContent">
+                  <NavLink to="/">Accueil</NavLink>
+                </li>
+                <li className="navbarContent">
+                  <NavLink to="/Services">Prestations</NavLink>
+                </li>
+                <li className="navbarContent">
+                  <NavLink to="/Avis">Avis</NavLink>
+                </li>
+                <li className="navbarContent">
+                  <NavLink to="/About">Qui suis-je?</NavLink>
+                </li>
+              </nav>
+            </Menu>
+          </div>
+        </div>
       </section>
     </>
   );
